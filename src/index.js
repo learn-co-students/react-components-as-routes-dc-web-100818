374f34c058ccda4293ca8a3b90e993d5f553baef
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2018-12-26 16:02:45 -0500

    added componenets and login page with semantidc css;

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import NavBar from './NavBar'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
 
-ReactDOM.render(
-  <Home />,
+
+
+
+
+
+
+ReactDOM.render((
+  <Router>
+     <React.Fragment>
+       <NavBar />
+       <Route exact path="/" component={Home} />
+       <Route exact path="/about" component={About} />
+       <Route exact path="/login" component={Login} />
+     </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
